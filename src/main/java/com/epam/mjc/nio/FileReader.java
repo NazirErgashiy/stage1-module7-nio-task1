aaaddae9d71021598a7958989113a30115179031
NazirErgashiy <pikobiznes@gmail.com>
2022-12-20 17:23:19 +0500

    Completed task

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        String collector = "";
+
+        try (RandomAccessFile aFile = new RandomAccessFile(file, "r")) {
+            FileChannel fileChannel = aFile.getChannel();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+
+            while (fileChannel.read(byteBuffer) > 0) {
+                byteBuffer.flip();
+                for (int i = 0; i < byteBuffer.limit(); i++) {
+                    collector += (char) byteBuffer.get();
+                }
+            }
+            byteBuffer.clear();
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        String regex[] = collector.split(System.getProperty("line.separator"));
+        String name = regex[0].replace("Name: ", "");
+        String age = regex[1].replace("Age: ", "");
+        String email = regex[2].replace("Email: ", "");
+        String phone = regex[3].replace("Phone: ", "");
+
+        return new Profile(name, Integer.parseInt(age), email, Long.parseLong(phone));
     }
 }
